fed53642c6206fc76bbc2698cf7637422e59f27c
赵笑乐 <zhaoxiaole@heweather.com>
2019-08-01 17:45:09 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div id="he-plugin-standard"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      settings: ''
+    }
+  },
+  mounted() {
+    let settings = '<script> const WIDGET = { CONFIG: { "layout": 1, "width": 450, "height": 150, "background": 1, "dataColor": "FFFFFF", "borderRadius": 5, "key": "Your Key" } } <\/script>';
+    $(settings).appendTo(document.body);
+    $('<script src="https://widget.heweather.net/standard/static/js/he-standard-common.js?v=1.1" type="text/javascript"><\/script>').appendTo(document.body);
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
